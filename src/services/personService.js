adb817bba51f3a0d296bc1e35eacc5ef694dba9f
eman467 <aziz.eman@hotmail.com>
2025-12-25 13:27:39 -0500

    env backend api url

--- src/services/personService.js
+++ src/services/personService.js
@@ -1,26 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
-// const baseUrl = import.meta.env.VITE_BACKEND_API
-const baseUrl = 'http://localhost:3000'
+const baseUrl = import.meta.env.VITE_BACKEND_API;
+// const baseUrl = "http://localhost:3000";
 
 const getAll = () => {
-    const request = axios.get(`${baseUrl}/api/persons`)
-    return request.then(response => response.data)
-}
+  const request = axios.get(`${baseUrl}/api/persons`);
+  return request.then((response) => response.data);
+};
 
 const create = (newPerson) => {
-    const request = axios.post(`${baseUrl}/api/persons`, newPerson)
-    return request.then(response => response.data)
-}
+  const request = axios.post(`${baseUrl}/api/persons`, newPerson);
+  return request.then((response) => response.data);
+};
 
 const destroy = (id) => {
-    const request = axios.delete(`${baseUrl}/api/persons/${id}`)
-    return request.then(response => response)
-}
+  const request = axios.delete(`${baseUrl}/api/persons/${id}`);
+  return request.then((response) => response);
+};
 
 const update = (id, updatedPerson) => {
-    const request = axios.put(`${baseUrl}/api/persons/${id}`, updatedPerson)
-    return request.then(response => response.data)
-}
+  const request = axios.put(`${baseUrl}/api/persons/${id}`, updatedPerson);
+  return request.then((response) => response.data);
+};
 
-export default {getAll, create, destroy, update}
+export default { getAll, create, destroy, update };
